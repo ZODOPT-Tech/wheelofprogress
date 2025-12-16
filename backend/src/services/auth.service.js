1e69b064720ba588767bdb9ead5185126869830f
Siddarthdevanga <101501191+Siddarthdevanga@users.noreply.github.com>
2025-12-16 12:38:42 +0530

    Update auth.service.js

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -1,5 +1,4 @@
 import * as adminRepo from "../repositories/admin.repo.js";
-import bcrypt from "bcrypt";
 
 export const login = async (email, password) => {
   const admin = await adminRepo.findByEmail(email);
@@ -8,16 +7,8 @@ export const login = async (email, password) => {
     throw new Error("User not found");
   }
 
-  const isValid = await bcrypt.compare(password, admin.password_hash);
-  if (!isValid) {
-    throw new Error("Invalid credentials");
-  }
-
-  return {
-    message: "Login successful",
-    adminId: admin.id,
-    companyId: admin.company_id,
-  };
+  return admin;
 };
+
 
 
