d942a3568e680cc700152c1ebb2b93bb4f4936b2
Siddarthdevanga <101501191+Siddarthdevanga@users.noreply.github.com>
2025-12-16 12:19:37 +0530

    Update page.js

--- frontend/app/auth/login/page.js
+++ frontend/app/auth/login/page.js
@@ -38,17 +38,18 @@ export default function Login() {
       return;
     }
 
-    try {
-      const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/login`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email, password }),
-        }
-      );
+   try {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/auth/login`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    }
+  );
+
 
       const data = await res.json();
 
