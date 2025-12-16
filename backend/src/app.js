31ab98ab003a96711bd7959fb10386468feda888
Siddarthdevanga <101501191+Siddarthdevanga@users.noreply.github.com>
2025-12-16 13:00:20 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,25 +1,13 @@
 import express from "express";
-import cors from "./config/cors.js";
-
+import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
-import companyRoutes from "./routes/company.routes.js";
-import visitorRoutes from "./routes/visitor.routes.js";
-import dashboardRoutes from "./routes/dashboard.routes.js";
-import paymentRoutes from "./routes/payment.routes.js";
-
-import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use(cors);
 
 app.use("/api/auth", authRoutes);
-app.use("/api/company", companyRoutes);
-app.use("/api/visitors", visitorRoutes);
-app.use("/api/dashboard", dashboardRoutes);
-app.use("/api/payment", paymentRoutes);
-
-app.use(errorMiddleware);
 
 export default app;
+
