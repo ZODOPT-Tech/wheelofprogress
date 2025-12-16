80084eeb6eb8275ce7f1a06026e472a6f28bc0ef
Siddarthdevanga <101501191+Siddarthdevanga@users.noreply.github.com>
2025-12-16 12:33:33 +0530

    Update admin.repo.js

--- backend/src/repositories/admin.repo.js
+++ backend/src/repositories/admin.repo.js
@@ -1,13 +1,11 @@
-import { getDB } from "../config/db.js";
+import db from "../config/db.js";
 
-export const findAdminByEmail = async (email) => {
-  const db = await getDB();
-  const [[row]] = await db.query(
-    `SELECT a.*, c.payment_status, c.is_active
-     FROM company_admins a
-     JOIN companies c ON a.company_id=c.id
-     WHERE a.admin_email=?`,
+export const findByEmail = async (email) => {
+  const [rows] = await db.query(
+    `SELECT * FROM company_admins WHERE email = ? LIMIT 1`,
     [email]
   );
-  return row;
+
+  return rows.length ? rows[0] : null;
 };
+
