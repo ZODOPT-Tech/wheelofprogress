46b5fe4764ba4d3db6acdacf82e5097adfded7bb
Siddarthdevanga <101501191+Siddarthdevanga@users.noreply.github.com>
2025-12-16 12:59:12 +0530

    Update auth.routes.js

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,9 +1,10 @@
-import express from "express";
+import { Router } from "express";
 import { login } from "../controllers/auth.controller.js";
 
-const router = express.Router();
+const router = Router();
 
 router.post("/login", login);
 
 export default router;
+
 
