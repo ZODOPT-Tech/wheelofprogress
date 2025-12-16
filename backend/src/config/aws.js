689704ac5c3f1836b1d065ccc2a16dd5f78b72b6
Siddarthdevanga <101501191+Siddarthdevanga@users.noreply.github.com>
2025-12-16 12:40:55 +0530

    Update aws.js

--- backend/src/config/aws.js
+++ backend/src/config/aws.js
@@ -1,24 +1,18 @@
 import AWS from "aws-sdk";
-import { AWS_REGION, AWS_SECRET_ARN } from "./env.js";
 
-const secretsManager = new AWS.SecretsManager({
-  region: AWS_REGION
+AWS.config.update({
+  region: process.env.AWS_REGION,
 });
 
-export const getSecrets = async () => {
-  const res = await secretsManager.getSecretValue({
-    SecretId: AWS_SECRET_ARN
-  }).promise();
+const secretsManager = new AWS.SecretsManager();
 
-  return JSON.parse(res.SecretString);
+export const getSecret = async () => {
+  const data = await secretsManager
+    .getSecretValue({
+      SecretId: process.env.AWS_SECRET_ARN,
+    })
+    .promise();
+
+  return JSON.parse(data.SecretString);
 };
 
-export const getS3Client = async () => {
-  const secrets = await getSecrets();
-
-  return new AWS.S3({
-    region: AWS_REGION,
-    accessKeyId: secrets.AWS_ACCESS_KEY_ID,
-    secretAccessKey: secrets.AWS_SECRET_ACCESS_KEY
-  });
-};
