8cdda29ee75a7647b0f01bb054157477ff384b68
Siddarthdevanga <101501191+Siddarthdevanga@users.noreply.github.com>
2025-12-16 12:43:19 +0530

    Update aws.js

--- backend/src/config/aws.js
+++ backend/src/config/aws.js
@@ -1,11 +1,21 @@
 import AWS from "aws-sdk";
 
+/**
+ * Global AWS config
+ */
 AWS.config.update({
   region: process.env.AWS_REGION,
 });
 
+/**
+ * Clients
+ */
 const secretsManager = new AWS.SecretsManager();
+const s3 = new AWS.S3();
 
+/**
+ * 🔐 Get secrets from AWS Secrets Manager
+ */
 export const getSecret = async () => {
   const data = await secretsManager
     .getSecretValue({
@@ -15,4 +25,12 @@ export const getSecret = async () => {
 
   return JSON.parse(data.SecretString);
 };
+
+/**
+ * 📦 Get S3 client
+ */
+export const getS3Client = () => {
+  return s3;
+};
+
 
